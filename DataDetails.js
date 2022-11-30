00ba13b49c2bbbcffff3b92754a625852722dd1a
Maximiliano Gartner <maxi-gartner@hotmail.com>
2022-11-30 00:24:55 -0300

    :tada: Create repository

---
+++ DataDetails.js
@@ -0,0 +1,46 @@
+console.log("testing")
+
+const cards = document.getElementById("cards")
+console.log("cards",cards)
+
+const API_URL = "https://jsonplaceholder.typicode.com/users";
+
+const getUser = async() => {
+    const getFetch = await fetch(API_URL)
+    const getData = await getFetch.json()
+    //console.log("getData",getData)
+    addCards(getData)
+}
+getUser()
+
+const cardDetail = new URLSearchParams(window.location.search)
+    const params = cardDetail.get('name')
+    console.log(params)
+
+    const addCards = getData => {
+        const filterData = getData.filter(item => item.name.replace(/\s/g,'') === params)
+        console.log("filterData",filterData)
+
+        const template = document.querySelector("#template-cards").content;
+        const fragment = document.createDocumentFragment();
+    
+        filterData.forEach((data) => {
+            template.querySelector('.card-header').textContent = data.name;
+            template.querySelector('.card-user').textContent = "User: "+data.username;
+            template.querySelector('.card-email').textContent = "Email: "+data.email;
+            template.querySelector('.card-telefon-number').textContent = "Telefon Number: "+data.phone;
+            template.querySelector('.card-website').textContent = "Website: "+data.website;
+            template.querySelector('.card-city').textContent = "City: "+data.address.city;
+            template.querySelector('.card-street').textContent = "Street: "+data.address.street;
+            template.querySelector('.card-suite').textContent = "Suite: "+data.address.suite;
+            template.querySelector('.card-zipcode').textContent = "Zipcode: "+data.address.zipcode;
+            template.querySelector('.card-nameCompany').textContent = "Company: "+data.company.name;
+            template.querySelector('.card-catchPhrase').textContent = data.company.catchPhrase;
+            template.querySelector('a').setAttribute("href", "./index.html");
+                
+                const clone = template.cloneNode(true);
+                fragment.appendChild(clone);
+        });
+        cards.appendChild(fragment);
+    }
+    addCards(getData)
---
+++ data.js
@@ -0,0 +1,40 @@
+console.log("testing")
+
+const cards = document.getElementById("cards")
+console.log("cards",cards)
+
+const API_URL = "https://jsonplaceholder.typicode.com/users";
+
+const getUser = async() => {
+    const getFetch = await fetch(API_URL)
+    const getData = await getFetch.json()
+    //console.log("getData",getData)
+    addCards(getData)
+    
+}
+getUser()
+
+console.log("hasta aca funciona")
+
+/**
+ * @returns crea las cards
+ */
+const addCards = getData => {
+    const template = document.querySelector("#template-cards").content;
+    const fragment = document.createDocumentFragment();
+
+    getData.forEach((data) => {
+        template.querySelector('.card-header').textContent = data.name;
+        template.querySelector('.card-user').textContent = "User: "+data.username;
+        template.querySelector('.card-email').textContent = "Email: "+data.email;
+        template.querySelector('.card-telefon-number').textContent = "Telefon Number: "+data.phone;
+        template.querySelector('a').setAttribute("href", `./details.html?name=${data.name.replace(/\s/g,'')}`);
+            
+            const clone = template.cloneNode(true);
+            fragment.appendChild(clone);
+    });
+    cards.appendChild(fragment);
+}
+addCards(getData)
+
+
